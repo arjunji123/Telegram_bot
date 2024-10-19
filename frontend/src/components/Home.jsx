8f329be374d463dc3bea413939a1600df11b708b
akanksha089 <achaturvdi089@gmail.com>
2024-10-19 19:05:25 +0530

    save

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -5,8 +5,7 @@ import Logo from "../utils/Logo";
 import Footer from "./Footer";
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchMeData, fetchCoinData } from '../../store/actions/homeActions';
-
-
+import { motion, AnimatePresence } from "framer-motion";
 
 function Home() {
   const [firstName, setFirstName] = useState("");
@@ -14,6 +13,7 @@ function Home() {
   const apiData = useSelector((state) => state.apiData.data);
   const userData = apiData && apiData.me && apiData.me.data  || null;
   const pendingCoin = apiData && apiData.coin && apiData.coin.data  || null;
+  const [coins, setCoins] = useState([]);
 
   useEffect(() => {
   dispatch(fetchCoinData());
@@ -30,6 +30,18 @@ function Home() {
   }, []);
  
 
+  const handleClick = () => {
+    const newCoins = Array.from({ length: 10 }, (_, i) => ({
+      id: Date.now() + i, // Unique ID
+      x: (Math.random() - 0.5) * 300, // Random x offset for movement
+      y: (Math.random() - 0.5) * 300, // Random y offset for movement
+    }));
+
+    setCoins(newCoins);
+
+    // Remove coins after animation
+    setTimeout(() => setCoins([]), 2500);
+  };
 
   return (
     <div className="bg-white flex justify-center">
@@ -57,16 +69,51 @@ function Home() {
               </p>
             </div>
 
-            {/* Profile Picture */}
-            <div className="px-4 my-6 cursor-pointer flex justify-center">
-              <img
-                src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSPzFN--8Y1W-1Yg9anA4ZXy-W18bIfJ-4RNZ8QWi6wPeGJUUoE"
-                alt="Main Character"
-                className="rounded-full w-56 h-56 object-cover"
-              />
-            </div>
+      
+     
+    {/* Coin Button and Image */}
+    <div className="coin-animation-container my-6 relative">
+          <motion.div
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+            className="coin-btn"
+            onClick={handleClick}
+          >
+            <img
+              src="https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSPzFN--8Y1W-1Yg9anA4ZXy-W18bIfJ-4RNZ8QWi6wPeGJUUoE"
+              alt="Main Character"
+              className="character-img"
+            />
+          </motion.div>
 
-            <div  className="w-8/12 border-2 border-[#f5eded] rounded-xl h-16 mx-auto flex justify-center items-center cursor-pointer">
+          {/* Hamster-style Coin Animation */}
+          <div className="coins-container flex justify-center items-center">
+            <AnimatePresence>
+              {coins.map((coin) => (
+                <motion.div
+                  key={coin.id}
+                  className="coin"
+                  initial={{ opacity: 0, scale: 0.5, x: 0, y: 0 }}
+                  animate={{
+                    opacity: 1,
+                    x: coin.x, // Move randomly in the X direction
+                    y: coin.y, // Move randomly in the Y direction
+                    rotate: [0, 360],
+                    scale: [1, 1.5, 1],
+                  }}
+                  exit={{ opacity: 0, scale: 0 }}
+                  transition={{ duration: 2, ease: "easeOut" }}
+                >
+                  <img src="src\images\dollar-coin.png" alt="Hamster Coin" className="coin-image w-6 h-6" /> {/* Replace with your hamster coin image */}
+                </motion.div>
+              ))}
+            </AnimatePresence>
+          </div>
+
+         
+          </div>
+
+             <div  className="w-8/12 border-2 border-[#f5eded] rounded-xl h-16 mx-auto flex justify-center items-center cursor-pointer">
             <p className="text-xl font-extrabold font-poppins text-[#f5eded]">Pending Coin 
               <span className="pl-2 text-2xl">
               {pendingCoin ? pendingCoin.pending_coin : '700,0000'}
