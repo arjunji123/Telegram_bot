1c276974c3e54269442feb58165d62ebbbb6af1c
Arjun Singh Naruka <89446608+arjunji123@users.noreply.github.com>
2024-11-11 10:42:14 +0530

    Update config.js

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -2,4 +2,4 @@
 export const BACKEND_URL = "https://telegram-bot-by30.onrender.com";
 
 export const FRONTEND_URL =
-  "https://unitradebot.onrender.com";
+  "https://telegram-bot-git-frontuni-arjuns-projects-e072bddd.vercel.app";
