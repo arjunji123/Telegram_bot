d0d38c4219a0a8bc6435e0edf4163b1dcf946e8c
akanksha089 <achaturvdi089@gmail.com>
2024-11-18 20:13:27 +0530

    save

--- frontend/src/components/PublicRoute.jsx
+++ frontend/src/components/PublicRoute.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 const PublicRoute = () => {
-  const isAuthenticated = Boolean(localStorage.getItem('token'));
+  const isAuthenticated = Boolean(localStorage.getItem('user'));
 
   return isAuthenticated ? <Navigate to="/home" /> : <Outlet />;
 };
