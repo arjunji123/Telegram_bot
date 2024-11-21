f21bd593f4f147d167f422fcf26fbd15b5b22a7b
akanksha089 <achaturvdi089@gmail.com>
2024-11-21 12:13:37 +0530

    save

--- frontend/src/components/Tasks.jsx
+++ frontend/src/components/Tasks.jsx
@@ -246,7 +246,7 @@ function Tasks() {
 
 
   return (
-    <div className="bg-white flex justify-center min-h-screen font-poppins   ">
+    <div className="bg-white flex justify-center min-h-screen font-poppins  ">
     <ToastContainer
       position="top-right"
       autoClose={5000}
@@ -258,7 +258,7 @@ function Tasks() {
     />
     {loading && <Loader />}
     
-    <div className="w-full bg-black text-white  flex flex-col max-w-lg  sm:mx-auto overflow-y-auto">
+    <div className="w-full bg-black text-white  flex flex-col max-w-lg h-screen sm:mx-auto overflow-y-auto ">
       <div className="flex-grow relative z-0">
         <div className="px-2 py-6 h-full z-10">
           <Logo />
@@ -278,7 +278,7 @@ function Tasks() {
   
           <h1 className="text-center text-2xl text-white shadow-lg font-bold font-poppins mt-4">COIN QUESTS</h1>
   
-          <div className="  ">
+          <div className=" h-fit mb-8 ">
             <div className="mt-4">
               {rows &&
                 rows.map((row, index) => (
