616b56f18bc43f5ef68cff55d606d351a7984742
akanksha089 <achaturvdi089@gmail.com>
2024-11-26 20:01:23 +0530

    save

--- frontend/src/components/Withdrawal.jsx
+++ frontend/src/components/Withdrawal.jsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef, useState } from "react";
 import Logo from "../utils/Logo";
 import QRCode from "qrcode";
-import {  BiSolidUpvote, BiHistory } from "react-icons/bi";
+import { BiSolidUpvote, BiHistory } from "react-icons/bi";
 import { BsPersonFillCheck, BsCurrencyRupee } from "react-icons/bs";
 import { AiFillCaretDown } from "react-icons/ai";
-import { RiVerifiedBadgeLine } from "react-icons/ri";
+import { IoMdThumbsUp } from "react-icons/io";
 import { useNavigate } from 'react-router-dom';
 import WithdrawCoin from "../utils/WithdrawCoin";
 import Footer from "./Footer";
@@ -14,7 +14,7 @@ import { useDispatch, useSelector } from 'react-redux';
 import { fetchAPIData, fetchMeData } from '../../store/actions/homeActions';
 import { shareCoins } from "../../store/actions/coinActions";
 import ShareCoin from "../utils/ShareCoin";
-import { toast, ToastContainer } from "react-toastify"; 
+import { toast, ToastContainer } from "react-toastify";
 import Loader from '../components/Loader';
 import Swal from "sweetalert2";
 import withReactContent from "sweetalert2-react-content";
@@ -30,8 +30,8 @@ function Withdrawal() {
   const [showHistoryPopup, setShowHistoryPopup] = useState(false);
   const [showWithdrawal, setShowWithdrawalPopup] = useState(false);
   const [sharePopup, setSharePopup] = useState(false);
-  const [selectedCoinRate, setSelectedCoinRate] = useState(null); 
-  const [company_id, setCompany_id] = useState(null); 
+  const [selectedCoinRate, setSelectedCoinRate] = useState(null);
+  const [company_id, setCompany_id] = useState(null);
   const toggleSharePopup = () => {
     setSharePopup(!sharePopup);
   };
@@ -39,7 +39,7 @@ function Withdrawal() {
     recipientReferralCode: '',
     amount: ''
   });
-  const { success} = useSelector((state) => ({
+  const { success } = useSelector((state) => ({
     success: state.coinData.success,
 
   }));
@@ -47,33 +47,33 @@ function Withdrawal() {
     toAddress: '',
     fromAddress: '',
     amount: '',
-});
-const [sellData, setSellData] = useState({
-  user_id: "",
-  company_id: "",
-  address: "",
-  amount: "",
-  coin_rate: "",
-});
+  });
+  const [sellData, setSellData] = useState({
+    user_id: "",
+    company_id: "",
+    address: "",
+    amount: "",
+    coin_rate: "",
+  });
   const dispatch = useDispatch();
-  const {  data, error } = useSelector((state) => state.moneyData);
+  const { data, error } = useSelector((state) => state.moneyData);
   const apiCompanies = useSelector((state) => state.apiData.data.apicompanies);
   const apiData = useSelector((state) => state.apiData.data);
-  const userData = apiData && apiData.me && apiData.me.data  || null;
-// console.log('apiCompanies', apiCompanies)
- useEffect(() => {
-   const fetchData = async () => {
-     try {
-       await dispatch(fetchAPIData('apiCompanies'));
-       await dispatch(fetchMeData());
-       setLoading(false); 
-     } catch (error) {
-       setLoading(false);   
-       console.error("Error fetching data", error);
-     }
-   };
-   fetchData();
- }, [dispatch]);
+  const userData = apiData && apiData.me && apiData.me.data || null;
+  // console.log('apiCompanies', apiCompanies)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        await dispatch(fetchAPIData('apiCompanies'));
+        await dispatch(fetchMeData());
+        setLoading(false);
+      } catch (error) {
+        setLoading(false);
+        console.error("Error fetching data", error);
+      }
+    };
+    fetchData();
+  }, [dispatch]);
 
   const handleIconClick = (index) => {
     setActiveIndex(index);
@@ -91,7 +91,7 @@ const [sellData, setSellData] = useState({
       setShowSendPopup(false);
       // setShowHistoryPopup(true);
       navigate('/history')
-    } 
+    }
   };
 
   const closePopups = () => {
@@ -132,68 +132,68 @@ const [sellData, setSellData] = useState({
     const { toAddress, fromAddress, amount } = receiveData;
     dispatch(receiveMoney(id, toAddress, fromAddress, amount));
     closePopups(); // Close popup after dispatching the action
-};
-const handleReceiveInputChange = (e) => {
-  const { name, value } = e.target;
-  setReceiveData((prevState) => ({
+  };
+  const handleReceiveInputChange = (e) => {
+    const { name, value } = e.target;
+    setReceiveData((prevState) => ({
       ...prevState,
       [name]: value,
-  }));
-};
+    }));
+  };
 
-const handleSendInputChange = (e) => {
-  const { name, value } = e.target;
-  setSendData((prevData) => ({
-    ...prevData,
-    [name]: value,
-  }));
-};
-const handleSendMoney = async () => {
-  if (!sendData.amount || !sendData.recipientReferralCode) {
-    toast.warn("Please fill in all fields.");
-    return;
-  }
+  const handleSendInputChange = (e) => {
+    const { name, value } = e.target;
+    setSendData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+  const handleSendMoney = async () => {
+    if (!sendData.amount || !sendData.recipientReferralCode) {
+      toast.warn("Please fill in all fields.");
+      return;
+    }
 
-  setLoading(true);
+    setLoading(true);
 
-  try {
-    await dispatch(shareCoins(sendData));
-    // toast.success("Coins sent successfully!");
-  } catch (error) {
-    toast.error("Failed to send coins.");
-  } finally {
-    setLoading(false);
-  }
-};
+    try {
+      await dispatch(shareCoins(sendData));
+      // toast.success("Coins sent successfully!");
+    } catch (error) {
+      toast.error("Failed to send coins.");
+    } finally {
+      setLoading(false);
+    }
+  };
 
-useEffect(() => {
-  // console.log('Success:', success);
-  // console.log('Error:', error);
-  
-  if (success) {
-    setSharePopup(false); // Close the popup on success
+  useEffect(() => {
+    // console.log('Success:', success);
+    // console.log('Error:', error);
 
-    // Reset the form state if needed
-    setSendData({
-      amount: '',
-      recipientReferralCode: '',
-    });
-  } else if (error) {
-  }
-}, [success, error]);
-const handleSellChange = (e) => {
-  const { name, value } = e.target;
-  setSellData((prev) => ({ ...prev, [name]: value }));
-};
+    if (success) {
+      setSharePopup(false); // Close the popup on success
 
-const handleSellSubmit = (e) => {
-  e.preventDefault();
+      // Reset the form state if needed
+      setSendData({
+        amount: '',
+        recipientReferralCode: '',
+      });
+    } else if (error) {
+    }
+  }, [success, error]);
+  const handleSellChange = (e) => {
+    const { name, value } = e.target;
+    setSellData((prev) => ({ ...prev, [name]: value }));
+  };
 
-  const user_id = "your_user_id"; // Replace with actual user_id
-  const company_id = "your_company_id"; // Replace with actual company_id
-  
-  dispatch(sellMoney(user_id, company_id, sellData));
-};
+  const handleSellSubmit = (e) => {
+    e.preventDefault();
+
+    const user_id = "your_user_id"; // Replace with actual user_id
+    const company_id = "your_company_id"; // Replace with actual company_id
+
+    dispatch(sellMoney(user_id, company_id, sellData));
+  };
 
   // SweetAlert function for attractive alert
   const showAlert = () => {
@@ -211,7 +211,7 @@ const handleSellSubmit = (e) => {
       },
     });
   };
-  
+
 
   // Handle Button Click
   const handleButtonClick = (coinRate, companyId) => {
@@ -228,116 +228,139 @@ const handleSellSubmit = (e) => {
     <>
 
       <div className="bg-white flex justify-center min-h-screen font-Inter">
-            <ToastContainer
-        position="top-right"
-        autoClose={5000}
-        hideProgressBar={false}
-        closeOnClick
-        pauseOnHover
-        draggable
-        theme="dark"
-      />
-      {loading ? (
-        <Loader />
-      ) :
-      <div className="w-full bg-black text-white flex flex-col max-w-lg px-4 ">
-        <div className="flex-grow relative z-0 pt-6  pb-16">
-          {/* <Logo /> */}
-          <div className="flex justify-center font-Inter  leading-3 space-x-1 text-[34px] font-extrabold mt-3 mb-4">
-            <p>U</p>
-            <p className="">{userData ? userData.coins : "0"}</p>
-          </div>
-
-           <div className="grid grid-cols-2 gap-2 mb-4">
-      {/* Send Button */}
-      <div
-        onClick={() => toggleSharePopup()}
-        className="text-white mx-auto cursor-pointer flex flex-col items-center transition duration-300 ease-in-out opacity-100"
-      >
-        <div className="rounded-full w-8 h-8 bg-[#303030] flex justify-center items-center">
-          <BiSolidUpvote size={22} />
-        </div>
-        <span className="text-xs text-center font-Inter">Send</span>
-      </div>
-
-      {/* History Button */}
-      <div
-        onClick={() => handleIconClick(2)}
-        className="text-white mx-auto cursor-pointer flex flex-col items-center transition duration-300 ease-in-out opacity-100"
-      >
-        <div className="rounded-full w-8 h-8 bg-[#303030] flex justify-center items-center">
-          <BiHistory size={22} />
-        </div>
-        <span className="text-xs text-center font-Inter">History</span>
-      </div>
-    </div>
-
-          <p className="text-center font-Inter text-xs text-[#f5eded] mb-4">
-            Sell your points at your chosen price, anytime and anywhere. Get instant cash withdrawals with no delays!
-          </p>
-
-          <hr className="border-gray-300 mb-4 w-full mx-auto" />
-
-          {/* Co-Companies List */}
-          <div className="flex flex-col space-y-2 ">
-            {apiCompanies && apiCompanies.data && apiCompanies.data.length > 0 ? (
-                 apiCompanies.data && apiCompanies.data.map((company, index) => (
-                  <div key={index} className="rounded-lg p-2 w-full  relative flex justify-between items-center bg-[#1b1a1a] transition duration-200 ease-in-out shadow-md">
-                    <div className="flex ">
-                      <BsPersonFillCheck size={18} />
-                      <div className="ml-1"> 
-                        <span className="text-[12px] font-semibold uppercase">{company.company_name}</span> {/* Adjusted name size */}
-                        <p className="font-bold flex  font-Inter items-center text-[17px] "> 
-                          <BsCurrencyRupee className="" />
-                          <span>{company.coin_rate}</span>
-                        </p>
-                        <h3 className="text-[10px] uppercase text-[#d3cece] ">limit 20k-80k uni coin</h3>
-                      </div>
-                      <RiVerifiedBadgeLine size={16} className="text-green-500 " /> {/* Reduced icon size */}
-                    </div>
-    
-                    <button
-                      className="leading-none px-2 py-1 text-xs rounded-md bg-red-600 flex text-white font-semibold hover:bg-red-500 transition duration-200 ease-in-out"
-                      onClick={() => handleButtonClick (company.coin_rate, company.company_id)}
-                    >
-                      <AiFillCaretDown size={16} /> {/* Reduced icon size */}
-                      <span className="ml-1">Sell</span>
-                    </button>
-                  </div>
-            )
-         
-            )): ""}
-          </div>
-        </div>
-
-
-      </div>
-}
-      <Footer />
-    
-      {
-        showWithdrawal && <WithdrawCoin toggleWithdrawalPopup={toggleWithdrawalPopup} handleReceiveMoney={handleReceiveMoney} handleReceiveInputChange={handleReceiveInputChange} 
-          receiveData={receiveData} />
-      }
-      {
-        showPopup && <Sell togglePopup={togglePopup} handleSellChange={handleSellChange} handleSellSubmit={handleSellSubmit}
-        coinRate={selectedCoinRate} userData={userData} company_id={company_id} />
-      }
-       {
-        sharePopup && <ShareCoin
-        toggleSharePopup= {toggleSharePopup}
-        handleSendInputChange={handleSendInputChange}
-        handleSendMoney={handleSendMoney}
-        sendData={sendData}
-        setSendData={setSendData}
+        <ToastContainer
+          position="top-right"
+          autoClose={5000}
+          hideProgressBar={false}
+          closeOnClick
+          pauseOnHover
+          draggable
+          theme="dark"
         />
-      }
-      {
-        showHistoryPopup && <History closePopups={closePopups}  />
-      }
-    </div>
+        {loading ? (
+          <Loader />
+        ) :
+          <div className="w-full bg-black text-white flex flex-col max-w-lg px-4 ">
+            <div className="flex-grow relative z-0 pt-6  pb-16">
+              {/* <Logo /> */}
+              <div className="flex justify-center font-Inter  leading-3 space-x-1 text-[34px] font-extrabold mt-3 mb-4">
+                <p>U</p>
+                <p className="">{userData ? userData.coins : "0"}</p>
+              </div>
+
+              <div className="grid grid-cols-2 gap-2 mb-4">
+                {/* Send Button */}
+                <div
+                  onClick={() => toggleSharePopup()}
+                  className="text-white mx-auto cursor-pointer flex flex-col items-center transition duration-300 ease-in-out opacity-100"
+                >
+                  <div className="rounded-full w-8 h-8 bg-[#303030] flex justify-center items-center">
+                    <BiSolidUpvote size={22} />
+                  </div>
+                  <span className="text-xs text-center font-Inter">Send</span>
+                </div>
+
+                {/* History Button */}
+                <div
+                  onClick={() => handleIconClick(2)}
+                  className="text-white mx-auto cursor-pointer flex flex-col items-center transition duration-300 ease-in-out opacity-100"
+                >
+                  <div className="rounded-full w-8 h-8 bg-[#303030] flex justify-center items-center">
+                    <BiHistory size={22} />
+                  </div>
+                  <span className="text-xs text-center font-Inter">History</span>
+                </div>
+              </div>
+
+              <p className="text-center font-Inter text-xs text-[#f5eded] mb-4">
+                Sell your points at your chosen price, anytime and anywhere. Get instant cash withdrawals with no delays!
+              </p>
+
+              <hr className="border-gray-300 mb-4 w-full mx-auto" />
+
+              {/* Co-Companies List */}
+              <div className="flex flex-col space-y-4">
+                {apiCompanies && apiCompanies.data && apiCompanies.data.length > 0 ? (
+                  apiCompanies.data && apiCompanies.data.map((company, index) => (
+                    <div key={index} className="py-2">
+                      <div className="flex justify-between items-center">
+                        <div className="flex items-center gap-2">
+                          <div className="w-6 h-6 bg-sky-400 rounded-full flex items-center justify-center">
+                            <span className="text-sm font-bold ">{company.company_name.charAt(0)}</span>
+                          </div>
+                          <span className="text-sm font-medium">{company.company_name}</span>
+                        </div>
+                        <div className="text-sm font-medium text-[#5B5A5C]">123 Orders (30D) | 98%</div>
+                      </div>
+                      <div className="flex justify-between items-center py-2">
+                        <p className="font-bold flex  font-Inter items-center  ">
+                          <BsCurrencyRupee className="" />  <span className="text-[20px] ">{company.coin_rate}</span></p>
+                        <div className="text-sm font-medium text-[#5B5A5C]">Online</div>
+                      </div>
+                      <div className="flex justify-between items-start">
+                        <div className="leading-tight">
+                          <p>
+                            <span className="text-xs font-bold text-[#5B5A5C]">Quantity</span>
+                            <span className="text-xs font-medium ml-2 text-[#B8B7BA]">300 USDT</span>
+                          </p>
+                          <p>
+                            <span className="text-xs font-bold text-[#5B5A5C]">Limit</span>
+                            <span className="text-xs font-medium ml-2 text-[#B8B7BA]">22,000 - 22,000 INR</span>
+                          </p>
+                        </div>
+                        <p className="flex items-start text-green-500 gap-0.5 ">
+                          <IoMdThumbsUp size={24} className="pt-1"/>
+                          <span className="text-sm font-medium leading-loose">100%</span>
+                        </p>
+                      </div>
+                      <div className="flex justify-between items-center py-2 ">
+                        <div className="">
+
+                        </div>
+                        <button
+                          className="leading-none px-4 py-2 text-sm rounded-full bg-red-600 flex text-white font-semibold hover:bg-red-500 transition duration-200 ease-in-out"
+                          onClick={() => handleButtonClick(company.coin_rate, company.company_id)}
+                        >Sell
+                        </button>
+                      </div>
+
+
+                    </div>
+
+                  )
+
+                  )) : ""}
+              </div>
+            </div>
+
+
+          </div>
+        }
+        <Footer />
+
+        {
+          showWithdrawal && <WithdrawCoin toggleWithdrawalPopup={toggleWithdrawalPopup} handleReceiveMoney={handleReceiveMoney} handleReceiveInputChange={handleReceiveInputChange}
+            receiveData={receiveData} />
+        }
+        {
+          showPopup && <Sell togglePopup={togglePopup} handleSellChange={handleSellChange} handleSellSubmit={handleSellSubmit}
+            coinRate={selectedCoinRate} userData={userData} company_id={company_id} />
+        }
+        {
+          sharePopup && <ShareCoin
+            toggleSharePopup={toggleSharePopup}
+            handleSendInputChange={handleSendInputChange}
+            handleSendMoney={handleSendMoney}
+            sendData={sendData}
+            setSendData={setSendData}
+          />
+        }
+        {
+          showHistoryPopup && <History closePopups={closePopups} />
+        }
+      </div>
     </>
-  
+
 
 
 
