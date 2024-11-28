4d4c13a5c2c2baa158f0a939e787f83668c2ccf9
akanksha089 <achaturvdi089@gmail.com>
2024-11-28 18:33:23 +0530

    save

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,6 +11,12 @@ export default {
       fontFamily: {
         Inter: ["Inter", "sans-serif"], // Corrected syntax for font-family
       },
+      padding: {
+        'safe-bottom': 'env(safe-area-inset-bottom)',
+        'safe-top': 'env(safe-area-inset-top)',
+        'safe-left': 'env(safe-area-inset-left)',
+        'safe-right': 'env(safe-area-inset-right)',
+      },
     },
   },
   plugins: [],
