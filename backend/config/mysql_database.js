6db3ab5a00fd0ed0169fb3b46694d80c35f8afb1
Arjun Singh Naruka <89446608+arjunji123@users.noreply.github.com>
2024-12-12 15:27:38 +0530

    Update mysql_database.js

--- backend/config/mysql_database.js
+++ backend/config/mysql_database.js
@@ -1,3 +1,35 @@
+// const mysql = require("mysql2/promise");
+// const dotenv = require("dotenv");
+// dotenv.config({ path: "backend/config/config.env" });
+
+// const mysqlPool = mysql.createPool({
+//   host: process.env.DB_HOST,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+//   port: process.env.DB_PORT,
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+
+// module.exports = mysqlPool;
+
+// const mysql = require("mysql2/promise");
+// const dotenv = require("dotenv");
+// dotenv.config({ path: "backend/config/config.env" });
+
+// const mysqlPool = mysql.createPool({
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "unitradebot",
+//   waitForConnections: true,
+//   connectionLimit: 10,
+//   queueLimit: 0,
+// });
+// module.exports = mysqlPool;
+
 const mysql = require("mysql2/promise");
 const dotenv = require("dotenv");
 dotenv.config({ path: "backend/config/config.env" });
@@ -15,17 +47,3 @@ const mysqlPool = mysql.createPool({
 
 module.exports = mysqlPool;
 
-// const mysql = require("mysql2/promise");
-// const dotenv = require("dotenv");
-// dotenv.config({ path: "backend/config/config.env" });
-
-// const mysqlPool = mysql.createPool({
-//   host: "localhost",
-//   user: "root",
-//   password: "",
-//   database: "unitradebot",
-//   waitForConnections: true,
-//   connectionLimit: 10,
-//   queueLimit: 0,
-// });
-// module.exports = mysqlPool;
