724d052623d43da1a0ef960c0fefc4a107eab3f5
Akanksha <112845076+akanksha089@users.noreply.github.com>
2024-12-20 17:26:49 +0530

    Update app.js
    edeaser

--- backend/app.js
+++ backend/app.js
@@ -22,7 +22,7 @@ const localStorage = new LocalStorage("./scratch");
 app.use(
   cors({
     origin:
-      "https://telegram-bot-git-frontuni-arjuns-projects-e072bddd.vercel.app",
+      "https://unitrade-hub.vercel.app/",
     methods: ["GET", "POST", "PATCH","PUT"],
     credentials: true,
   })
